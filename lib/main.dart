116e202f5021b11c9bc19b3c353c063fa2272f57
DESKTOP-BQ8581G\Windows 10 <peeradol75@gmail.com>
2025-02-25 06:43:12 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:fam_care/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app_routes.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
